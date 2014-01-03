e5748b2abfa0c1d4016eb6dd566c6ced4f446e7d
lipp0050 <lipp0050@umn.edu>
2014-01-02 21:39:18 -0600

    updated security, added tloop.sh and template.pl, added some more packages

--- ubuntu_fresh_bootstrap.sh
+++ ubuntu_fresh_bootstrap.sh
@@ -5,18 +5,54 @@
 # Check run command as a login shell
 # Change to built-in scheme of Green on Black
 # Slide the transparent bar approximately 80% to the right
+# Set indicator-multiload to automatically startup... I could've probably
+#    written a script to do it... and some day probably will...
+# Go Applications -> System Tools -> System Settings -> Privacy - adjust as desired
 
-# Firefox should have the extensions 'Tamper Data 11.0.1' 'Web Developer 1.2.5' installed
+# Firefox should have the extensions 'Tamper Data 11.0.1' 'Web Developer 1.2.5' 'Adblock Plus' 'Ghostery' 'HTTPS-Everywhere' installed
+
+####  Special thanks to everyone on the internet who posted obscure details about some 
+####  small thing that they figured out. Without all of those contributions, this script
+####  would have never been realized. Also, thanks to the open source community for
+####  continuing to produce amazing software that only gets better each year!
+
+#### This script is designed for the Lenovo x230t. It should run on any computer, supported by Ubuntu 12.04 LTS however.
+#### If you are not running a Lenovo x230t, simply remove the section that installs Magick Rotation. Later I'll write
+#### a wrapper prompting for that info.
+
+# Start the firewall
+
+sudo ufw default deny
+
+sudo ufw enable
+
+sudo ufw status verbose
+
+# Disable the guest account
+
+cd /etc/lightdm
+
+echo 'allow-guest=false' >> lightdm.conf
+
+cd
 
 # Get the basic packages up and running
 
 sudo apt-get update && sudo apt-get upgrade -y
 
-for i in wireshark keepassx virtualbox arduino fceu blender inkscape gimp scribus ufraw dropbox mysql-workbench qtcreator smartgit audacity clementine vim gnucash git nmap gnome-session-fallback mysql-server tomcat7 apache2 xournal curl imagemagick tmux dkms python2.7 cellwriter; do
-    sudo apt-get -y install $i
+for PACKAGE in shred scrub wireshark keepassx virtualbox arduino fceu blender inkscape gimp scribus ufraw dropbox mysql-workbench qtcreator smartgit audacity clementine vim gnucash git nmap gnome-session-fallback mysql-server tomcat7 apache2 xournal curl imagemagick tmux dkms python2.7 cellwriter  indicator-multiload; do
+    sudo apt-get -y install $PACKAGE
 done
 
 sudo apt-get -y install build-essential make libglib2.0-dev
+
+# Get pulseaudio equalizer running
+
+sudo add-apt-repository ppa:nilarimogard/webupd8 -y
+
+sudo apt-get update
+
+sudo apt-get -y install pulseaudio-equalizer
 
 # Get Chrome Running
 
@@ -70,7 +106,7 @@ sudo add-apt-repository ppa:webupd8team/java -y
 
 sudo apt-get update
 
-sudo apt-get install oracle-java7-installer
+sudo apt-get -y install oracle-java7-installer
 
 # Get STS-Spring Running
 
@@ -97,6 +133,12 @@ Categories=Development;IDE;Java;' > STS.desktop
 cd
 
 # Setup Configurations
+
+# Get Wireshark Running properly
+
+sudo dpkg-reconfigure wireshark-common
+
+sudo usermod -a -G wireshark $USER
 
 # Vim configuration
 
@@ -202,6 +244,88 @@ set-option -g display-panes-colour colour166 #orange
 
 set-window-option -g clock-mode-colour colour64 #green' > .tmux.conf
 
+# Install some scripts
+
+cd ~/bin
+
+echo '#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+######################################################
+# Author: lipp0050
+# Email: baz@foo.bar
+#
+# Created: 10/23/13
+# Last modified: 10/23/13
+#
+# Description:
+#
+# This is a program for creating a loose
+# perl based template
+######################################################
+
+
+if (!defined $ARGV[0]) { # Was a filename defined at the start of the program?
+    die "Please enter a filename when you execute the program.\n"
+}
+
+my ($input) = ($ARGV[0] =~ m/(^[^\W]*)/); # Strip ARGV[0] of any
+                                          # characters, after the first
+                                          # nonword character
+
+my $new_filename = $input . '.pl'; # Append '.pl' to the new filename
+
+if (-e $new_filename) { # Do not overwrite an existing file with the same name
+        die "Filename already exists.\n"
+}
+
+open FILE_FH, '>', $new_filename;
+
+# Set up the programs environment
+print FILE_FH "#!/usr/bin/env perl\n\n",
+              "use strict;\n",
+              "use warnings;\n",
+              "use autodie;\n\n";
+
+# Set up programmer's details
+print FILE_FH "#" x 54, "\n",
+              "# Author: YOUR NAME\n", # Enter your name here
+              "# Email: YourName\@foo.bar\n", # Enter your email here
+              "#\n",
+              "# Created:\n",
+              "# Last modified:\n",
+              "#\n",
+              "# Description:\n",
+              "#\n",
+              "#\n",
+              "#" x 54, "\n";
+
+close FILE_FH;
+
+chmod 0755, "$new_filename"; # Make the program executable
+
+system ("vim $new_filename");' >> template.pl
+
+sudo chmod 0755 template.pl
+
+echo '#!/bin/bash
+
+while true; do
+tmux select-window -t "$sn:5"
+sleep 5
+tmux select-window -t "$sn:6"
+sleep 5
+tmux select-window -t "$sn:7"
+sleep 5
+done' >> tloop.sh
+
+sudo chmod 0755 tloop.sh
+
+cd
+
 # Config bash
 
 echo 'alias ls="ls --color"
@@ -210,6 +334,7 @@ alias vi="vim"' >> .bashrc
 # Make sure we grabbed everything
 
 sudo apt-get -fy install
+
 sudo apt-get update && sudo apt-get upgrade -y
 
 # Finish
